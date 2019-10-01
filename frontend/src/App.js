4d75aa6a0cd3045b4236c8937dc764a122dbb65c
hans <hantc350@student.liu.se>
2019-10-01 18:02:35 +0200

    add frontend code

---
+++ frontend/src/App.js
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { apiResponse: "" };
+  }
+
+  callAPI() {
+    fetch("http://localhost:9000/testAPI")
+      .then(res => res.text())
+      .then(res => this.setState({ apiResponse: res }));
+  }
+
+  componentWillMount() {
+    this.callAPI();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <p>{this.state.apiResponse}</p>
+        <img
+          src={require("./static/cp.jpg")}
+          alt="cp"
+          width="328"
+          height="328"
+        />
+        <p>Tjenare, mitt namn är Christopher, hue hue!</p>
+      </div>
+    );
+  }
+}
+
+export default App;
