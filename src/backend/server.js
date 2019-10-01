d9ad0d25ddfd048d0b4a02ae677efcf6fe07edd8
hans <hantc350@student.liu.se>
2019-10-01 17:06:32 +0200

    add merge

--- src/backend/server.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const app = express();
-var cors = require("cors");
-
-const PORT = 4000;
-
-app.use(cors());
-
-app.listen(PORT, () => console.log("Server is running on Port: ", PORT));
