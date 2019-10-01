6fbc05470ebcb581af63aec92353e8fb72b36c6f
hans <hantc350@student.liu.se>
2019-10-01 16:52:10 +0200

    add prettier

--- backend/routes/testBackend.js
+++ backend/routes/testBackend.js
@@ -2,7 +2,7 @@ var express = require("express");
 var router = express.Router();
 
 router.get("/", function(req, res, next) {
-    res.send("Backend is working properly!") ;
+  res.send("Backend is working properly!");
 });
 
 module.exports = router;
